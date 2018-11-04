6c405a0d48f6c2cad26ba0f2b38ca8dab37a0f11
Shan Lalani <slalani@vols.utk.edu>
2018-11-04 09:15:12 -0600

    Add files via upload

--- backup/js/notes.js
+++ backup/js/notes.js
@@ -1,31 +1,35 @@
-function createNoteCard(name) {
+var course = localStorage.getItem('course');
+
+function createNoteCard(name, rating) {
     var div = document.createElement('div');
     var div2 = document.createElement('div');
     var span = document.createElement('span');
     var span2 = document.createElement('span');
     var element = document.getElementById('notes');
-
+ 
     div.classList.add("card");
     div.classList.add("hoverable");
     div2.classList.add("card-content");
     span.classList.add("card-title");
     span2.classList.add("card-rating");
     span.innerHTML = name;
-
+ 
     // TO be determined with info from firebase (rating)
-    // var starRating = 
-
+    // var starRating =
+ 
     var iTags = []
-    for (let i = 0; i < 5; /* i < starRating; */ i++) {
+    for (let i = 0; i < rating; /* i < starRating; */ i++) {
       var j = document.createElement('i');
       j.classList.add('material-icons');
       j.innerHTML = "stars";
+      j.style.color = '#fbc02d';
+    //   j.classList.add('yellow');
       iTags[i] = j;
     }
-
-    div.setAttribute("onclick", "nextPage()");
+ 
+    div.addEventListener("click", nextPage.bind(null, name));
     div.style.cursor = 'pointer';
-
+ 
     div.appendChild(div2);
     div2.appendChild(span);
     for (let i=0; i < iTags.length; i++) {
@@ -33,12 +37,13 @@ function createNoteCard(name) {
     }
     element.appendChild(div);
 }
-
+ 
 function nextPage() {
     window.location = 'pdfdisplay.html';
 }
-
+ 
 var notes = ["Jolene's CS101 Notes", "Peter's MATH101 Notes", "Tim's MUSI134 Notes"];
+var ratings = [5, 4, 2];
 for (let i=0; i < notes.length; i++) {
-    createNoteCard(notes[i]);
+    createNoteCard(notes[i], ratings[i]);
 }
